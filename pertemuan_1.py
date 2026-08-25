8d723415356981a3c162033959511bd781bb9c78
AnantaKharisma <anankharis12@gmail.com>
2026-08-25 20:03:16 +0700

    Add files via upload

---
+++ pertemuan_1.py
@@ -0,0 +1,7 @@
+print("\n");       #\n digunakan untuk membuat baris baru
+print("\t--- Identitas Diri ---");      #\t digunakan untuk membuat tab
+print("\tNama: Ananta Kharisma");
+print("\tNIM: 2605060037");
+print("\tTTL: Magelang, 12 September 2006");
+print("\tJenis Kelamin: Laki-laki");
+print("\tHobi: futsal, renang, dan bermain game");
